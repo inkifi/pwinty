b3fc87322156f002b1d0982fef236e1c542ff672
Dmitry Fedyuk <dfediuk@gmail.com>
2019-03-13 16:56:32 +0600

    no message

--- Settings.php
+++ Settings.php
@@ -1,24 +1,8 @@
 <?php
 namespace Inkifi\Pwinty;
-use Magento\Framework\App\ScopeInterface as S;
-use Magento\Store\Model\Store;
 // 2019-03-13
 /** @method static Settings s() */
-final class Settings extends \Df\Config\Settings {
-	/**
-	 * 2019-03-13 «Merchant ID»
-	 * @param null|string|int|S|Store|array(string, int) $s [optional]
-	 * @return string
-	 */
-	function id($s = null) {return $this->v('merchant_id', $s);}
-
-	/**
-	 * 2019-03-13 «API KEY»
-	 * @param null|string|int|S|Store|array(string, int) $s [optional]
-	 * @return string
-	 */
-	function key($s = null) {return $this->v('pwinty_api_key', $s);}
-
+final class Settings extends \Df\API\Settings {
 	/**
 	 * 2019-03-13
 	 * @override
