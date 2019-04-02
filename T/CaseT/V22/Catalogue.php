0eec84ed3478d64bfc7628fc23bfe6417cfa1026
Dmitry Fedyuk <dfediuk@gmail.com>
2019-04-02 16:52:36 +0600

    no message

--- T/CaseT/V22/Catalogue.php
+++ T/CaseT/V22/Catalogue.php
@@ -7,7 +7,46 @@ final class Catalogue extends \Inkifi\Pwinty\T\CaseT\V22 {
 	/** @test 2019-04-02 */
 	function t00() {}
 
-	/** @test 2019-04-02 */
+	/**
+	 * @test 2019-04-02
+	 * A response:
+	 * {
+	 *		"country": "UNITED KINGDOM",
+	 *		"countryCode": "GB",
+	 *		"errorMessage": null,
+	 *		"items": {
+	 *			"0": {
+	 *				"attributes": [],
+	 *				"description": "10x10 inch print",
+	 *				"errorMessage": null,
+	 *				"fullProductHorizontalSize": 10,
+	 *				"fullProductVerticalSize": 10,
+	 *				"imageHorizontalSize": 10,
+	 *				"imageVerticalSize": 10,
+	 *				"itemType": "Print",
+	 *				"name": "10x10",
+	 *				"priceGBP": 200,
+	 *				"priceUSD": 261,
+	 *				"recommendedHorizontalResolution": 1500,
+	 *				"recommendedVerticalResolution": 1500,
+	 *				"shippingBand": "Prints",
+	 *				"sizeUnits": "inches"
+	 *			},
+	 *			<...>
+	 *		},
+	 *		"qualityLevel": "Pro",
+	 *		"shippingRates": [
+	 *			{
+	 *				"band": "LargePrints",
+	 *				"description": "1st Class Royal Mail",
+	 *				"isTracked": false,
+	 *				"priceGBP": 399,
+	 *				"priceUSD": 521
+	 *			},
+	 *			<...>
+	 *		]
+	 *	}
+	 */
 	function t01() {
 		$s = S::s(); /** @var S $s */
 		$api = new API([
