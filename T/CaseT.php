b341664a52f3ab7e3f55d25204014eb08ad3170f
Dmitry Fedyuk <dfediuk@gmail.com>
2019-04-17 03:32:40 +0600

    no message

--- T/CaseT.php
+++ T/CaseT.php
@@ -2,7 +2,6 @@
 namespace Inkifi\Pwinty\T;
 /**
  * 2019-04-02
- * @see \Inkifi\Pwinty\T\CaseT\AvailableForDownload
  * @see \Inkifi\Pwinty\T\CaseT\V22\Order
  * @see \Inkifi\Pwinty\T\CaseT\V26\Catalogue
  * @see \Inkifi\Pwinty\T\CaseT\V26\Order\AddImage
--- T/CaseT/V26/Catalogue.php
+++ T/CaseT/V26/Catalogue.php
@@ -3,15 +3,14 @@ namespace Inkifi\Pwinty\T\CaseT\V26;
 use Inkifi\Pwinty\API\B\Catalogue as bCatalogue;
 // 2019-04-06
 final class Catalogue extends \Inkifi\Pwinty\T\CaseT {
-	/** @test 2019-04-06 */
+	/** 2019-04-06 */
 	function t00() {}
 
-	/** 2019-04-06 */
+	/** 2019-04-06 https://www.pwinty.com/api/2.6/#products-list */
 	function t01() {echo df_json_encode(bCatalogue::p(df_order(60055)->getStore()));}
 
 	/**
 	 * 2019-04-02
-	 * «Create an order» https://www.pwinty.com/api/2.2/#orders-create
 	 * An item with attributes:
 	 *	{
 	 *		"attributes": [
