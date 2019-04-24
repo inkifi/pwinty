b10245107e987750ee3fa4633aa515baf6b31a27
Dmitry Fedyuk <dfediuk@gmail.com>
2019-04-24 17:47:14 +0600

    no message

--- T/CaseT/V30/Order/AddImage.php
+++ T/CaseT/V30/Order/AddImage.php
@@ -47,7 +47,7 @@ final class AddImage extends \Inkifi\Pwinty\T\CaseT {
 		,'url' => 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/Cat_poster_1.jpg/1024px-Cat_poster_1.jpg'
 	])->a());}
 
-	/** @test 2019-04-24 */
+	/** 2019-04-24 */
 	function t05() {echo df_json_encode(bAddImage::p(bCreate::p(df_order(60055)), [
 		'attributes' => []
 		,'copies' => 1
@@ -55,4 +55,30 @@ final class AddImage extends \Inkifi\Pwinty\T\CaseT {
 		,'sku' => 'FRA-INSTA-30X30'
 		,'url' => 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/Cat_poster_1.jpg/1024px-Cat_poster_1.jpg'
 	])->id());}
+
+	/** @test 2019-04-24
+	 * A response:
+	 *	{
+	 *		"attributes": null,
+	 *		"copies": 1,
+	 *		"errorMessage": null,
+	 *		"id": 988488,
+	 *		"md5Hash": null,
+	 *		"previewUrl": null,
+	 *		"price": 0,
+	 *		"priceToUser": null,
+	 *		"sizing": "Crop",
+	 *		"sku": "FRA-INSTA-30X30",
+	 *		"status": "NotYetDownloaded",
+	 *		"thumbnailUrl": null,
+	 *		"url": "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/Cat_poster_1.jpg/1024px-Cat_poster_1.jpg"
+	 *	}
+	 */
+	function t06() {echo df_json_encode(bAddImage::p(bCreate::p(df_order(60055)), [
+		'attributes' => []
+		,'copies' => 1
+		,'sizing' => 'Crop'
+		,'sku' => 'FRA-INSTA-30X30'
+		,'url' => 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/Cat_poster_1.jpg/1024px-Cat_poster_1.jpg'
+	])->a());}
 }
