1465952a78ac7b6e7cf39fd46b780e25f518c895
Dmitry Fedyuk <dfediuk@gmail.com>
2019-04-24 18:08:07 +0600

    no message

--- T/CaseT/V30/Order/AddImages.php
+++ T/CaseT/V30/Order/AddImages.php
@@ -5,10 +5,10 @@ use Inkifi\Pwinty\API\B\Order\Create as bCreate;
 use Inkifi\Pwinty\API\Entity\Image as eImage;
 // 2019-04-24
 final class AddImages extends \Inkifi\Pwinty\T\CaseT {
-	/** 2019-04-24 */
+	/** @test 2019-04-24 */
 	function t00() {}
 
-	/** @test 2019-04-24 */
+	/** 2019-04-24 */
 	function t01() {echo df_json_encode(array_map(
 		function(eImage $i) {return $i->a();}
 		,bAddImages::p(bCreate::p(df_order(60055)), [
@@ -27,26 +27,7 @@ final class AddImages extends \Inkifi\Pwinty\T\CaseT {
 		])
 	));}
 
-	/**
-	 * 2019-04-24
-	 *	[
-	 *		{
-	 *			"attributes": {"frame_colour": "gold"},
-	 *			"copies": 1,
-	 *			"errorMessage": null,
-	 *			"id": 986984,
-	 *			"md5Hash": null,
-	 * 			"previewUrl": null,
-	 *			"price": 9750,
-	 *			"priceToUser": null,
-	 *			"sizing": "Crop",
-	 *			"status": "NotYetDownloaded",
-	 *			"thumbnailUrl": null,
-	 *			"type": "BoxFrame_24x48_Unmounted",
-	 *			"url": "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/Cat_poster_1.jpg/1024px-Cat_poster_1.jpg"
-	 *		}
-	 *	]
-	 */
+	/** 2019-04-24 «Invalid attribute name frame_colour» */
 	function t02() {echo df_json_encode(array_map(
 		function(eImage $i) {return $i->a();}
 		,bAddImages::p(bCreate::p(df_order(60055)), [
@@ -54,7 +35,7 @@ final class AddImages extends \Inkifi\Pwinty\T\CaseT {
 				->attributes(['frame_colour' => 'gold'])
 				->copies(1)
 				->sizing('Crop')
-				->sku('BoxFrame_24x48_Unmounted')
+				->sku('FRA-INSTA-30X30')
 				->url('https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/Cat_poster_1.jpg/1024px-Cat_poster_1.jpg')
 		])
 	));}
