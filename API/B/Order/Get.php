bd51a4031bec35d32933d12e22b0c0512330415f
Dmitry Fedyuk <dfediuk@gmail.com>
2020-02-06 16:30:02 +0600

    no message

--- API/B/Order/Get.php
+++ API/B/Order/Get.php
@@ -7,8 +7,8 @@ use Inkifi\Pwinty\API\Facade\Order as F;
 final class Get {
 	/**
 	 * 2019-04-08 https://www.pwinty.com/api/2.6/#orders-get
-	 * function@used-by \Inkifi\Pwinty\T\CaseT\V30\Order\Get::t01()
-	 * function@used-by \Inkifi\Pwinty\T\CaseT\V30\Order\Submit::t02()
+	 * @used-by \Inkifi\Pwinty\T\CaseT\V30\Order\Get::t01()
+	 * @used-by \Inkifi\Pwinty\T\CaseT\V30\Order\Submit::t02()
 	 * @param eOrder $o
 	 * @return eOrder
 	 */
