eb41577a4b437b21eb09785e65beb9e4d08845ef
Dmitry Fedyuk <dfediuk@gmail.com>
2020-02-29 11:03:09 +0600

    no message

--- API/Client.php
+++ API/Client.php
@@ -37,7 +37,7 @@ final class Client extends \Df\API\Client {
 	 * 2019-04-04
 	 * @override
 	 * @see \Df\API\Client::responseValidatorC()
-	 * @used-by \Df\API\Client::p()
+	 * @used-by \Df\API\Client::_p()
 	 * @return string
 	 */
 	protected function responseValidatorC() {return \Inkifi\Pwinty\API\Validator::class;}
